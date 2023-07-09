3acbfd5702b6bc7e5839c526aefd1ea778a4257f
AniaPiWo <snowflake@wp.pl>
2023-07-09 17:08:18 +0200

    feat: grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,63 @@
 import "./App.css";
+import React, { useState } from "react";
 import Counter from "./components/Counter/Counter";
 import SingleLight from "./components/SingleLight/SingleLight";
 import Text from "./components/Text/Text";
+import Grid from "./components/Grid/Grid";
 
 function App() {
+  const [rows, setRows] = useState(0);
+  const [columns, setColumns] = useState(0);
+  const totalLights = rows * columns;
+  const lights = Array.from({ length: totalLights });
+
+  const handleDecrementRows = () => {
+    if (rows <= 0) {
+      return;
+    }
+    setRows(rows - 1);
+  };
+
+  const handleIncrementRows = () => {
+    setRows(rows + 1);
+  };
+
+  const handleDecrementColumns = () => {
+    if (columns <= 0) {
+      return;
+    }
+    setColumns(columns - 1);
+  };
+
+  const handleIncrementColumns = () => {
+    if (columns >= 9) {
+      return;
+    }
+    setColumns(columns + 1);
+  };
+
   return (
     <div>
       <nav className="nav">
-        <Text>0 bulbs</Text>
-        <Counter text={"rows"} count={"0"} />
-        <Counter text={"columns"} count={"0"} />
+        <Text>{totalLights} bulbs</Text>
+        <Counter
+          text={"rows"}
+          count={rows}
+          onIncrement={handleIncrementRows}
+          onDecrement={handleDecrementRows}
+        />
+        <Counter
+          text={"columns"}
+          count={columns}
+          onIncrement={handleIncrementColumns}
+          onDecrement={handleDecrementColumns}
+        />
       </nav>
-      <div>
-        <SingleLight />
-      </div>
+      <Grid columns={columns}>
+        {lights.map((value, index) => {
+          return <SingleLight key={index} />;
+        })}
+      </Grid>
     </div>
   );
 }
